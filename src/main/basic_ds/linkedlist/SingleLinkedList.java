e1c7c33d5028fcd0db2f71222e3e5fe0ab9a571e
Carson417 <2230904623@qq.com>
2024-12-30 18:22:21 +0800

    提交单链表的一些操作

---
+++ src/main/basic_ds/linkedlist/SingleLinkedList.java
@@ -0,0 +1,141 @@
+package main.basic_ds.linkedlist;
+
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+/**
+ * 单链表
+ * 1.在链表头添加
+ * 2.在链表尾添加（找最后一个节点）
+ * 3.遍历：（1）for (2)while (3)迭代器
+ */
+public class SingleLinkedList implements Iterable<Integer> {
+
+    // 头指针
+    private Node head;
+
+    // 当内部类不需要访问外部类的实例成员时，可以将其声明为 static
+    private static class Node {
+        int value;
+        Node next;
+
+        // 定义内部类好处：实现隐藏细节
+        Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+
+    /**
+     * 链表头部添加元素
+     */
+    public void addFirst(int value) {
+        // case1:链表为空
+        // 为什么是head而不是head.next,因为此时head是null，会空指针
+        // 是head指向这个第一个元素，引用，其实就是一个，而不是顺序的下一个元素,把head当引用而不是指针
+        // head = new Node(value, null);
+
+        // case2:链表中非空
+        // 可以处理上种情况
+        head = new Node(value, head);
+    }
+
+    /**
+     * 在链表尾添加元素
+     */
+    public void addLast(int value) {
+        Node last = findLast();
+        if (last == null) {
+            addFirst(value);
+            return;
+        }
+        last.next = new Node(value, null);
+    }
+
+    // 找最后一个节点
+    private Node findLast() {
+        Node p = head;
+        if (p == null) {    // 不判断会有空指针
+            return p;
+        }
+        while (p.next != null) {
+            p = p.next;
+        }
+        return p;
+    }
+
+    /**
+     * 遍历链表方法1：for
+     */
+    public void loop_for(Consumer<Integer> consumer) {
+        for (Node node = head; node != null; node = node.next) {
+            consumer.accept(node.value);
+        }
+    }
+
+    /**
+     * 遍历链表方法2：while
+     */
+    public void loop_while(Consumer<Integer> consumer) {
+        Node node = head;
+        while (node != null) {
+            consumer.accept(node.value);
+            node = node.next;
+        }
+    }
+
+    /**
+     * 遍历链表方法3:迭代器
+     */
+    @Override
+    public Iterator<Integer> iterator() {
+        // 匿名内部类，为什么不加static，因为需要外部类的成员变量
+        return new Iterator<Integer>() {
+            Node node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public Integer next() {
+                int value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
