ec0400c895045a3e45480527f82ac9a2711d62f4
Carson417 <2230904623@qq.com>
2025-01-03 18:05:15 +0800

    提交排序的一些操作

---
+++ src/ds_al/basic_al/sort/InsertionSort.java
@@ -0,0 +1,18 @@
+package ds_al.basic_al.sort;
+
+/**
+ * 插入排序
+ * 就像打牌一样
+ * 每次从未排序左边界取一张，插入到左侧已排序的牌中
+ */
+public class InsertionSort {
+
+    public void sort(int[]a){
+
+    }
+
+
+    private void Insertion(int[]a){
+
+    }
+}
