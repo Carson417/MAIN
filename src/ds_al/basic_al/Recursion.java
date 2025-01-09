b4e5029d8877c6f5ad064234b298fb53806b128d
Carson417 <2230904623@qq.com>
2025-01-09 18:48:29 +0800

    提交一些操作

--- src/ds_al/basic_al/Recursion.java
+++ src/ds_al/basic_al/Recursion.java
@@ -6,6 +6,14 @@ import java.util.List;
  * 递归
  * 自己调自己
  * 递推关系： f(n) = f(n.next) 判断停止
+ * <p>
+ * 1.阶乘
+ * 2.反向打印字符串
+ * 3.二分查找
+ * 4.斐波那契
+ * 5.斐波那契变种：兔子问题
+ * 6.斐波那契变种：青蛙跳楼梯
+ * 7.斐波那契优化：记忆法/备忘录---剪枝
  */
 public class Recursion {
 
@@ -50,4 +58,80 @@ public class Recursion {
             return middle;
         }
     }
+
+
+    /**
+     * 斐波那契,求第n项
+     */
+    public int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    /**
+     * 斐波那契变种：兔子问题
+     * <p>
+     * 第一个月，一对未成熟的兔子
+     * 第二个月，成熟
+     * 第三个月及以后，他们每个月能产下一对新兔子
+     * 所有兔子遵循此规律，求第n个月的兔子数
+     * <p>
+     * 递归关系：以第6月举例
+     * 第6月的兔子 = 第5月的那些兔子 + 第5月成熟兔子生的兔子
+     * 第5月成熟兔子生的兔子 = 第5月成熟兔子 = 第四月的兔子
+     * 所以 f(n) = f(n-1) + f(n-2)
+     */
+    public int fibonacci_rabbit(int n) {
+        if (n == 1 || n == 2) {
+            return 2;
+        }
+        return fibonacci_rabbit(n - 1) + fibonacci_rabbit(n - 2);
+    }
+
+    /**
+     * 斐波那契变种：青蛙跳楼梯
+     * <p>
+     * 楼梯有n阶
+     * 青蛙要跳到楼顶，一次可以跳一阶或者两阶，只能向上跳
+     * 问有多少种跳法
+     * <p>
+     * 递归关系：f(n) = f(n-1) + f(n-2)
+     * 第n阶的种数 = 第n-1阶种数（最后一次跳1阶）+ 第n-2阶种数（最后一次跳2阶）
+     */
+    public int fibonacci_jumpStair(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        return fibonacci_jumpStair(n - 1) + fibonacci_jumpStair(n - 2);
+    }
+
+    /**
+     * 斐波那契优化：备忘录--剪枝
+     */
+//    public int fibonacci_memo(int n) {
+//        if (n == 0) {
+//            return 0;
+//        }
+//        if (n == 1) {
+//            return 1;
+//        }
+//
+//        int[] memo = new int[n];
+//        memo[0] = 0;
+//        memo[1] = 1;
+//        memo[n-1]=fibonacci(n);
+//
+//        if()
+//
+//
+//        return fibonacci(n - 1) + fibonacci(n - 2);
+//    }
 }
--- src/ds_al/basic_al/sort/InsertionSort.java
+++ src/ds_al/basic_al/sort/InsertionSort.java
@@ -7,12 +7,31 @@ package ds_al.basic_al.sort;
  */
 public class InsertionSort {
 
-    public void sort(int[]a){
-
+    public void sort(int[] a) {
+        // 默认 0 索引是排好序的
+        Insertion1(a,1);
     }
 
 
-    private void Insertion(int[]a){
+    /**
+     * 实现1
+     * @param low 未排序的下边界
+     */
+    private void Insertion1(int[] a, int low) {
+        if (low == a.length) {
+            return;
+        }
+        // 已排序区域指针
+        int i = low - 1;
+        // 找到插入位置
+        while (i >= 0 && a[i] > a[low]) {
+            a[i + 1] = a[i];
+            i--;
+        }
+        // 插入   --- 为什么是i+1：找到时i--了，所以这里再加回来
+        a[i + 1] = a[low];
 
+        Insertion1(a, low + 1);
     }
+
 }
